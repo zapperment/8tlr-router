6db647f26626cf9079859f0768165c3b5756187e
Patrick Hund <wiekatz@gmail.com>
2024-09-30 16:38:10 +0200

    whatever

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
-// @ts-check
-
-import eslint from "@eslint/js";
+import globals from "globals";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-// files and directories we do NOT want to lint
-// (glob pattern)
-const ignores = [".yarn", ".yarn.lock", ".pnp.cjs", ".vscode"];
-
-const additionalConfig = { ignores };
-
-export default tseslint.config(
-  eslint.configs.recommended,
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  additionalConfig,
-);
+];
